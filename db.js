cb0d77e4888e3dcc45f02eb6bb4561d5659db543
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2026-02-24 21:33:27 -0600

    Ultimos Cambios

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const { Pool } = pkg;
 export const pool = new Pool({
     user: 'postgres',
     password: 'Alesonic111.',
-    host: 'db.aqirbybomcgoxhefftnx.supabase.co',
+    host: 'localhost',
     port: 5432,
-    database: 'postgres'
+    database: 'dbFuturo'
 });
